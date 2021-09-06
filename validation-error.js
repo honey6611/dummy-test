5be9a71a3906982148dd4173c27bb369f95ffa57
honey6611 <honey_6611@yahoo.com>
2021-09-06 13:12:43 +0100

    Create validation-error

---
+++ validation-error.js
@@ -0,0 +1,18 @@
+ function validate(fds,institutionId,countryCodes){
+  
+   const retvalue = fds.map((item)=>{
+     Object.keys(item).forEach((key)=>{
+      item[key]={
+         value: item[key],
+         error: item[key] =='' ? 'Cannot be empty' : key =='Author'? '': 'some error message'
+       }
+     })
+     return item;
+   })
+
+     const output = {
+       hasError: true,
+       data: retvalue
+     }  
+   return output
+}
